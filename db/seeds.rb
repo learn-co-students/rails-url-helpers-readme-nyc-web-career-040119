783b5e330965e91040141d0fa357dd2349346724
Gamerman2001 <Tro12185@gmail.com>
2019-04-25 18:04:34 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: "Imaginary lines", description: "The way that the rain hits the pane")
+Post.create(title: "Imaginary lines2", description: "The way that the rain hits the pane")
+Post.create(title: "Imaginary lines3", description: "The way that the rain hits the pane")
+Post.create(title: "Imaginary lines4", description: "The way that the rain hits the pane")
